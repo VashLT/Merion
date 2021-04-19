b30348b4ef64a9f91e348b24f9bc624467e84af7
VashLT <mcvicksilvaa@gmail.com>
2021-04-18 23:19:34 -0500

    added hello world react component

---
+++ frontend/src/components/App.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect, useRef } from 'react';
+import Nav from 'react-bootstrap/Nav';
+
+export default function App() {
+    return (
+        <div className="App">
+            <h1>React main component</h1>
+        </div>
+    )
+}
