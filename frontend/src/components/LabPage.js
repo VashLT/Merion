615911e022d3cb35703ffc91bf12b507e8f623af
VashLT <mcvicksilvaa@gmail.com>
2021-04-26 19:10:15 -0500

    added components for labs and lab pages

--- frontend/src/components/LabPage.js
+++ frontend/src/components/LabPage.js
@@ -1,29 +1,50 @@
 import React from "react";
-import labs from "../labs"
+import labs from "../labs";
 
 function LabPage(props) {
+    console.log(props);
+
     if (!("id" in props)) {
-        return <h1>id not valid</h1>;
+        return (
+            <div className="lab" id="labPage">
+                <h1>Id not valid</h1>;
+            </div>
+        );
     }
-    if (labs['id'] !== props.id) {
-        return <h1>No lab with given id ()</h1>;
+    if (!labExists(props.id)) {
+        return (
+            <div className="lab" id="labPage">
+                <h1>No lab with given id</h1>;
+            </div>
+        );
     }
     return (
-        <div className="lab" id="labPage">
+        <div id="labPage">
             {getRenderedComponent(props.id)}
         </div>
-    )
+    );
 }
 
 function getRenderedComponent(id) {
     var content;
-    for (i = 0; i < labs.length; i++) {
+    console.log(labs);
+    for (let i = 0; i < labs.length; i++) {
         if (labs[i].id === id) {
             content = labs[i].content;
+            break;
         }
     }
     return content;
 
+}
+
+function labExists(id) {
+    for (let i = 0; i < labs.length; i++) {
+        if (labs[i].id === id) {
+            return true;
+        }
+    }
+    return false;
 }
 
 export default LabPage;
--- frontend/src/components/LabsPage.js
+++ frontend/src/components/LabsPage.js
@@ -20,18 +20,18 @@ function getLabPreviewHTML(lab, index) {
     }
 
     return (
-        // <div className="card" onClick={document.getElementById('t' + index).click()} id="labItem">
         <div className="card" id="labItem">
-            <div className="card-header d-flex flex-row justify-content-between clicky align-items-center" id={'h' + index}>
+            <div className="card-header d-flex flex-row justify-content-between clicky align-items-center" id={'h' + index} onClick={() => document.getElementById('t' + index).click()}>
                 <h5 style={{ textAlign: "left" }} id={'t' + index} data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                     {lab.title}
                 </h5>
                 <a className="btn btn-primary" href={'/labs/' + lab.id} role="button" target="_blank">READ</a>
             </div>
-            <div id="collapseOne" className="collapse show" aria-labelledby={'h' + index} data-parent="#accordion">
+            <div id="collapseOne" className="collapse" aria-labelledby={'h' + index} data-parent="#accordion">
                 <div className="card card-body darky">
                     <ul className="list-group list-group-flush">
-                        {lab.questions.map((lab, index) => loadQuestion(lab, index))}
+                        {<lab.questions />}
+                        {/* {lab.questions.map((lab, index) => loadQuestion(lab, index))} */}
                     </ul>
                 </div>
             </div>
