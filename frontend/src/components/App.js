7149af75b10f4e9d6c96295a204c79bb203ba675
VashLT <mcvicksilvaa@gmail.com>
2021-05-06 10:22:16 -0500

    small changed to app content

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Navbar, Nav, Button, Form, FormControl } from 'react-bootstrap';
+import MerionNavbar from './Navbar';
 import {
     BrowserRouter as Router,
     Switch,
@@ -21,29 +21,7 @@ function App() {
     return (
         <div id="app">
             <div>
-                <Navbar collapseOnSelect sticky="top" expand="lg" bg="dark" variant="dark" id="navbar">
-                    <Navbar.Brand href="/">
-                        <img
-                            alt=""
-                            src="../static/images/merion-170x66.png"
-                            width="140"
-                            height="50"
-                            className="d-inline-block align-center merion-logo"
-                        />{' '}
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="mr-auto">
-                            <Nav.Link href="/">Hogar</Nav.Link>
-                            <Nav.Link href="/members">Miembros</Nav.Link>
-                            <Nav.Link href="/labs">Labs</Nav.Link>
-                        </Nav>
-                        <Nav>
-                            <Nav.Link href="#">Trabajo final</Nav.Link>
-                            <Nav.Link eventKey={2} href="#">Proyecto</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Navbar>
+                <MerionNavbar theme="dark" />
             </div>
             <Router>
                 <Switch>
