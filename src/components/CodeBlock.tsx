07215deff705d080ae1cf47e34a3b870c414ea60
VashLT <mcvicksilvaa@gmail.com>
2021-08-19 00:04:47 -0500

    added number lines

---
+++ src/components/CodeBlock.tsx
@@ -0,0 +1,42 @@
+import React, { useCallback, useState, useLayoutEffect } from 'react';
+
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { atomOneLight } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+
+const CodeBlock: React.FC<CodeBlockProps> = ({ filePath }) => {
+    const [fileContent, setFileContent] = useState<string | null>(null);
+
+    const loadFile = useCallback(async () => {
+
+        const rawFileContent = await fetch(filePath)
+            .then(response => response.text());
+
+        rawFileContent.split(/\r?\n/).forEach((line, index) => console.log(`line: ${index}, content: ${line}`));
+
+        setFileContent(rawFileContent);
+
+    }, [setFileContent, filePath]);
+
+
+    useLayoutEffect(() => {
+        if (fileContent) return;
+        loadFile();
+
+    }, [fileContent, filePath])
+
+    fetch(filePath)
+        .then(response => response.text());
+
+    return (
+        <SyntaxHighlighter
+            language="javascript"
+            style={atomOneLight}
+            showLineNumbers={true}
+
+        >
+            {fileContent ? fileContent : ''}
+        </SyntaxHighlighter>
+    );
+}
+
+export default CodeBlock;
